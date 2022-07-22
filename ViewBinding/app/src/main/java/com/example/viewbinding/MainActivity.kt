ab5ed040c4da4ab5fc545c491a1286fa88111408
sgsk88 <sgsk88@nate.com>
2022-07-22 17:51:23 +0900

    view binding

--- ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.viewbinding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+//ViewBinding
+//Activity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.testText.text = "변경된 테스트"
+        binding.testText.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        /*
+        Null 안전, 유형안전
+         */
     }
 }
