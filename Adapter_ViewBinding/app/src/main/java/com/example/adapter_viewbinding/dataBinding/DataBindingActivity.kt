c98659e73fbc9999947e49b67cc63971cbed9e04
sgsk88 <sgsk88@nate.com>
2022-08-10 01:02:57 +0900

    Data, ViewBinding + Adapter

--- Adapter_ViewBinding/app/src/main/java/com/example/adapter_viewbinding/dataBinding/DataBindingActivity.kt
+++ Adapter_ViewBinding/app/src/main/java/com/example/adapter_viewbinding/dataBinding/DataBindingActivity.kt
@@ -3,6 +3,7 @@ package com.example.adapter_viewbinding.dataBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.adapter_viewbinding.R
 import com.example.adapter_viewbinding.databinding.ActivityDataBindingBinding
 
@@ -13,8 +14,28 @@ class DataBindingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        binding = DataBindingUtil.setContentView(R.layout.activity_data_binding)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
 
+        val array = ArrayList<String>()
+//
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+
+        val CustomDataBinding = CustomDataBinding(array)
+
+        val rv = binding.rv
+        rv.adapter = CustomDataBinding
+        rv.layoutManager = LinearLayoutManager(this)
 
 
     }
