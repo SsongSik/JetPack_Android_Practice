d0135d35791dd3049ddf9ac65fe83e5a2a0a04c5
sgsk88 <sgsk88@nate.com>
2022-08-23 23:11:06 +0900

    Coroutine / ViewModelScope

---
+++ 4_CoroutineViewModelScope/app/src/main/java/com/example/a4_coroutineviewmodelscope/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.a4_coroutineviewmodelscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/*
+간단한 코투린 / ViewModelScope
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("TEST", "START")
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d("TEST", "CoroutineScope START")
+            a()
+            b()
+            Log.d("TEST", "CoroutineScope END")
+        }
+        Log.d("TEST", "END")
+
+        //start -> end -> AP1 -> BP1 -> AP2 -> BP2
+        //이런식으로 예상할 수 있음
+    }
+
+    suspend fun a() {
+
+        //a작업은 오래 걸려서 2초정도
+        delay(1000)
+        Log.d("TEST", "AP1")
+
+        delay(1000)
+        Log.d("TEST", "AP2")
+    }
+
+    suspend fun b() {
+        //b작업은 오래 걸려서 2초정도
+        delay(1000)
+        Log.d("TEST", "BP1")
+
+        delay(1000)
+        Log.d("TEST", "BP2")
+    }
+}
